4f6fbe677ab19365455e231695ec9097a43e163a
EreboCore <omegazac989@gmail.com>
2025-05-14 13:22:50 +0200

    correction exo 11

--- exo_11.rb
+++ exo_11.rb
@@ -4,12 +4,19 @@ print ">"
 
 user_age = gets.chomp.to_i
 
-time_now = Time.now.year
 
-user_age.upto(time_now) do |age|
-    an_depuis = time_now - age
-    age_num = age - user_age
-    verbe =( age == time_now ) ? 'as' : 'avais'
-    adjectif =( an_depuis > 1) ? 'annee' : 'an'
-    puts " il a #{an_depuis} #{adjectif} , tu #{verbe} #{age_num}"
+# age.upto(user_age) do |age|
+#     an_depuis = user_age - age
+#     age_num = age - user_age
+#     verbe =( age == user_age ) ? 'as' : 'avais'
+#     adjectif =( an_depuis > 1) ? 'annee' : 'an'
+#     puts " il a #{an_depuis} #{adjectif} , tu #{verbe} #{age_num}"
+# end
+
+1.upto(user_age) do |age|
+    past_years = user_age - age 
+
+    verbe =( age == user_age ) ? 'as' : 'avais'
+    adjectif =( past_years > 1) ? 'annee' : 'an'
+    puts " Il y a #{past_years} #{adjectif}, tu #{verbe} #{age} ans"
 end
