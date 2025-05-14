44cccbdf2735dd68e283592b367c6637b8ce9c3f
EreboCore <omegazac989@gmail.com>
2025-05-14 14:39:27 +0200

    correc 14

--- exo_14.rb
+++ exo_14.rb
@@ -1,10 +1,14 @@
-emails = []
+array = []
 
 1.upto(50) do |i|
-  emails << "jean.dupont#{i}@email.com"
+    if i < 10 
+        array << "jean.dupont.0#{i}@gmail.com"
+    else 
+        array << "jean.dupont.#{i}@gmail.com"
+    end
 end
 
-emails.each do |email|
+array.each do |email|
   numero = email[/\d+/].to_i
   puts email if numero.even?
 end
